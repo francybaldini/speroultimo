d447e2618d231ca2ea8139554e656a26fa0cc27c
francybaldini <francybaldini1312@gmail.com>
2026-06-04 23:39:59 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,103 @@
+/*hongjoong*/
+const openBtn = document.getElementById("openPopupHJ");
+const closeBtn = document.getElementById("ClosePopup");
+const popup = document.getElementById("OpenPopup1");
+
+openBtn.addEventListener("click", () => {
+    popup.classList.add("start");
+});
+
+closeBtn.addEventListener("click", () => {
+    popup.classList.remove("start");
+});
+
+/*seonghwa*/
+const openBtn2 = document.getElementById("openPopupSH");
+const closeBtn2 = document.getElementById("ClosePopup2");
+const popup2 = document.getElementById("OpenPopup2");
+
+openBtn2.addEventListener("click", () => {
+    popup2.classList.add("start");
+});
+
+closeBtn2.addEventListener("click", () => {
+    popup2.classList.remove("start");
+});
+
+/*yunho*/
+const openBtn3 = document.getElementById("openPopupYH");
+const closeBtn3 = document.getElementById("ClosePopup3");
+const popup3 = document.getElementById("OpenPopup3");
+
+openBtn3.addEventListener("click", () => {
+    popup3.classList.add("start");
+});
+
+closeBtn3.addEventListener("click", () => {
+    popup3.classList.remove("start");
+});
+
+/*yeosang*/
+const openBtn4 = document.getElementById("openPopupYS");
+const closeBtn4 = document.getElementById("ClosePopup4");
+const popup4 = document.getElementById("OpenPopup4");
+
+openBtn4.addEventListener("click", () => {
+    popup4.classList.add("start");
+});
+
+closeBtn4.addEventListener("click", () => {
+    popup4.classList.remove("start");
+});
+
+/*san*/
+const openBtn5 = document.getElementById("openPopupSN");
+const closeBtn5 = document.getElementById("ClosePopup5");
+const popup5 = document.getElementById("OpenPopup5");
+
+openBtn5.addEventListener("click", () => {
+    popup5.classList.add("start");
+});
+
+closeBtn5.addEventListener("click", () => {
+    popup5.classList.remove("start");
+});
+
+/*mingi*/
+const openBtn6 = document.getElementById("openPopupMN");
+const closeBtn6 = document.getElementById("ClosePopup6");
+const popup6 = document.getElementById("OpenPopup6");
+
+openBtn6.addEventListener("click", () => {
+    popup6.classList.add("start");
+});
+
+closeBtn6.addEventListener("click", () => {
+    popup6.classList.remove("start");
+});
+
+/*wooyoung*/
+const openBtn7 = document.getElementById("openPopupWY");
+const closeBtn7 = document.getElementById("ClosePopup7");
+const popup7 = document.getElementById("OpenPopup7");
+
+openBtn7.addEventListener("click", () => {
+    popup7.classList.add("start");
+});
+
+closeBtn7.addEventListener("click", () => {
+    popup7.classList.remove("start");
+});
+
+/*JONGHO*/
+const openBtn8 = document.getElementById("openPopupJH");
+const closeBtn8 = document.getElementById("ClosePopup8");
+const popup8 = document.getElementById("OpenPopup8");
+
+openBtn8.addEventListener("click", () => {
+    popup8.classList.add("start");
+});
+
+closeBtn8.addEventListener("click", () => {
+    popup8.classList.remove("start");
+});
