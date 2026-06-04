a421068e46c259eee1e4cdc849587f5aacaa6463
francybaldini <francybaldini1312@gmail.com>
2026-06-04 23:39:11 +0200

    Add files via upload

---
+++ discografia.js
@@ -0,0 +1,112 @@
+/*track 1 */
+const openbtn = document.getElementById("treasure1");
+const closebtn = document.getElementById("closepopup1");
+const popup = document.getElementById("openpopup1");
+
+openbtn.addEventListener("click", () =>{
+    popup.classList.add("active");
+});
+
+closebtn.addEventListener("click",() =>{
+    popup.classList.remove("active");
+});
+
+/*track 2 */
+const openbtn2 = document.getElementById("treasure2");
+const closebtn2 = document.getElementById("closepopup2");
+const popup2 = document.getElementById("openpopup2");
+
+openbtn2.addEventListener("click", () =>{
+    popup2.classList.add("active");
+});
+
+closebtn2.addEventListener("click",() =>{
+    popup2.classList.remove("active");
+});
+
+/*track 3 */
+const openbtn3 = document.getElementById("zerofever");
+const closebtn3 = document.getElementById("closepopup3");
+const popup3 = document.getElementById("openpopup3");
+
+openbtn3.addEventListener("click", () =>{
+    popup3.classList.add("active");
+});
+
+closebtn3.addEventListener("click",() =>{
+    popup3.classList.remove("active");
+});
+
+
+/*track 4 */
+const openbtn4 = document.getElementById("zerofever2");
+const closebtn4 = document.getElementById("closepopup4");
+const popup4 = document.getElementById("openpopup4");
+
+openbtn4.addEventListener("click", () =>{
+    popup.classList.add("active");
+});
+
+closebtn4.addEventListener("click",() =>{
+    popup.classList.remove("active");
+});
+
+
+/*track 5 */
+const openbtn5 = document.getElementById("theworld");
+const closebtn5 = document.getElementById("closepopup5");
+const popup5 = document.getElementById("openpopup5");
+
+openbtn5.addEventListener("click", () =>{
+    popup5.classList.add("active");
+});
+
+closebtn5.addEventListener("click",() =>{
+    popup5.classList.remove("active");
+});
+
+
+/*track 6 */
+const openbtn6 = document.getElementById("outlaw");
+const closebtn6 = document.getElementById("closepopup6");
+const popup6 = document.getElementById("openpopup6");
+
+openbtn6.addEventListener("click", () =>{
+    popup6.classList.add("active");
+});
+
+closebtn6.addEventListener("click",() =>{
+    popup6.classList.remove("active");
+});
+
+
+/*track 7 */
+const openbtn7 = document.getElementById("fin");
+const closebtn7 = document.getElementById("closepopup7");
+const popup7 = document.getElementById("openpopup7");
+
+openbtn7.addEventListener("click", () =>{
+    popup7.classList.add("active");
+});
+
+closebtn7.addEventListener("click",() =>{
+    popup7.classList.remove("active");
+});
+
+
+/*track 8 */
+const openbtn8 = document.getElementById("gh4");
+const closebtn8 = document.getElementById("closepopup8");
+const popup8 = document.getElementById("openpopup8");
+
+openbtn8.addEventListener("click", () =>{
+    popup8.classList.add("active");
+});
+
+closebtn8.addEventListener("click",() =>{
+    popup8.classList.remove("active");
+});
+
+
+
+
